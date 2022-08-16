fb34808fd420f2033c267f127871e8ab3579afb7
RaksaMenolakSadar <rakhshamuhammad@gmail.com>
2022-08-16 11:24:11 +0700

    fifth commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,7 +19,7 @@
     <div class="row align-items-md-stretch">
       <div class="col-md-6">
         <div class="h-100 p-5 text-white bg-dark rounded-3">
-          <h2>Wanted to go to the homepage?</h2>
+          <h2>Want to go to the homepage?</h2>
           <p>Click the button bellow to go to the homepage</p>
           <a href="/" class="btn btn-outline-light" type="button">Homepage</a>
         </div>
