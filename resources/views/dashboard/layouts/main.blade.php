0c416c2db983d129122b31c5f250802f5797461a
RaksaMenolakSadar <rakhshamuhammad@gmail.com>
2022-08-19 15:53:12 +0700

    commit ketigabelas

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -41,5 +41,11 @@
         <script src="https://unpkg.com/feather-icons"></script>
         <script src="path/to/dist/feather.js"></script>
         <script>feather.replace()</script>
+
+        <!-- chart JavaScript -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     </body>
 </html>
